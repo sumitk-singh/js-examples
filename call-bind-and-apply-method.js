291b2cdab827a9dc6fea4543acf9068d966f3907
SUMIT KUMAR SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-06-02 23:37:23 +0530

    bind method added

--- call-bind-and-apply-method.js
+++ call-bind-and-apply-method.js
@@ -1,7 +1,7 @@
 // Call, bind and apply method
 
 //Call method
-//The call() method calls a function with a given this value and arguments provided individually.
+//The call method calls a function with a given this value and arguments provided individually.
 const personName = {
   name: "Erons",
   func: function () {
@@ -16,3 +16,35 @@ function moreDetails(age, hobby) {
 moreDetails.call(personName, 17, "cooking");
 
 //--------------------------------------------------
+//Bind method
+//the bind method Returns a new function whose this value is bound to the provided value.
+const person = {
+  name: "Erons",
+  greet: function () {
+    return `Hello ${this.name}`;
+  }
+};
+
+function greetPerson() {
+  console.log(this.greet());
+}
+
+const bindPerson = greetPerson.bind(person);
+
+bindPerson();
+
+//--------------------------------------------------
+//Apply method
+//Calls a function with a provided this value. Further arguments are provided as a single array.
+const erons = {
+  name: "Erons",
+  speech: function () {
+    return `Hi, I'm ${this.name}`;
+  }
+};
+
+function moreSpeech(age, hobby) {
+  console.log(`${this.speech()}, I'm ${age} years old and I love ${hobby}`);
+}
+
+moreSpeech.apply(erons, [17, "cooking"]);
