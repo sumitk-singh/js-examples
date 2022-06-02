efc8672250c4d9c8adcca75ca594b7b3774726c6
SUMIT KUMAR SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-06-03 00:03:53 +0530

    promises explained

--- promisses.js
+++ promisses.js
@@ -1,17 +1,17 @@
-// eg to clear the concept of promisses
+//A Promise is a JavaScript object that links producing code and consuming code
+// Example
 
 const p = new Promise((resolve, reject) => {
-    const a = 1 + 2;
-    if (a == 2) {
-        resolve("success");
-    } else {
-        reject("failed")
-    }
-})
-
+  const a = 1 + 2;
+  if (a == 2) {
+    resolve("success");
+  } else {
+    reject("failed");
+  }
+});
 
 p.then((message) => {
-    console.log("this is in then " + message);
+  console.log("this is in then " + message);
 }).catch((message) => {
-    console.log("this in catch " + message);
-})
+  console.log("this in catch " + message);
+});
