7752c193d4555e48f5635cbb20bbe55c6078aac3
SUMIT KUMAR SINGH <76554602+sumitk-singh@users.noreply.github.com>
2022-06-03 07:02:59 +0530

    promisses

--- promisses.js
+++ promisses.js
@@ -1,4 +1,11 @@
 //A Promise is a JavaScript object that links producing code and consuming code
+
+//A promise can be:
+//fulfilled - The action relating to the promise succeeded
+//rejected - The action relating to the promise failed
+//pending - Hasn't fulfilled or rejected yet
+//settled - Has fulfilled or rejected
+
 // Example
 
 const p = new Promise((resolve, reject) => {
